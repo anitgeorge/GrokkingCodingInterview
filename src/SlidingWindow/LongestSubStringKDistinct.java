121f2efb4091e5497fbb733e6a1e4a624cca2e59
Anit George <anitgeorge89@gmail.com>
2020-08-14 07:25:40 +0530

    Updating documentation

--- src/SlidingWindow/LongestSubStringKDistinct.java
+++ src/SlidingWindow/LongestSubStringKDistinct.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Given a string, find the length of the longest substring
  * in it with no more than K distinct characters.
  *
--- src/SlidingWindow/MaxSumSubarrayOfSizeK.java
+++ src/SlidingWindow/MaxSumSubarrayOfSizeK.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Given an array of positive numbers and a positive number ‘k’,
  * find the maximum sum of any contiguous subarray of size ‘k’.
  *
