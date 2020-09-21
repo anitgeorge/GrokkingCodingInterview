161e6a7e1b0707faea10664db7a8e5cce6666727
Anit George <anitgeorge89@gmail.com>
2020-09-21 09:44:43 +0530

    Adding comment

--- src/TopKElements/src/FrequencyStack.java
+++ src/TopKElements/src/FrequencyStack.java
@@ -1,3 +1,22 @@
+/**
+ *
+ * Design a class that simulates a Stack data structure,
+ * implementing the following two operations:
+ *
+ *     push(int num): Pushes the number ‘num’ on the stack.
+ *     pop(): Returns the most frequent number in the stack. If there is a tie,
+ *     return the number which was pushed later.
+ *
+ *     After following push operations:
+ *     push(1), push(2), push(3), push(2), push(1), push(2), push(5)
+ *
+ *     1. pop() should return 2, as it is the most frequent number
+ *     2. Next pop() should return 1
+ *     3. Next pop() should return 2
+ *
+ * @author anitgeorge
+ */
+
 import java.util.*;
 
 class Element{
--- src/TopKElements/src/TaskScheduler.java
+++ src/TopKElements/src/TaskScheduler.java
@@ -1,3 +1,18 @@
+/**
+ *
+ * You are given a list of tasks that need to be run,
+ * in any order, on a server. Each task will take one
+ * CPU interval to execute but once a task has finished,
+ * it has a cooling period during which it can’t be run again.
+ * If the cooling period for all tasks is ‘K’ intervals,
+ * find the minimum number of CPU intervals that the server
+ * needs to finish all tasks.
+ *
+ * If at any time the server can’t execute any task then it must stay idle.
+ *
+ * @author anitgeorge
+ */
+
 import java.util.*;
 
 public class TaskScheduler {
